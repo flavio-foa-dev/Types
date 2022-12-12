9b4b80ee54092fdc8425deeb3791b40e3ffa200d
Flavio Andrade <foa12381@gmail.com>
2022-12-12 14:28:56 -0300

    types interfaces

--- src/domain/brana/main.ts
+++ src/domain/brana/main.ts
@@ -1,3 +1,5 @@
+import { isObject } from "util";
+
 export class Person {
   name: string;
   age: number;
@@ -18,5 +20,23 @@ persons[0] = {name: "Joao", age:19}
 persons[1] = {name: "Joao", age:19}
 persons[2] = {name: "Joao", age:19}
 persons[3] = {name: "Joao", age:19}
+
+
+class People {
+  public name: string;
+  #age: number;
+  readonly email: string
+
+  constructor(name: string, age: number, email: string) {
+    this.name = name;
+    this.#age = age;
+    this.email = email;
+  }
+
+}
+
+const people = new People("Joao Silva", 40, "joao@isObject.com")
+console.log(people)
+//people.#age = 40
 
 
